6607df25ec452ca5b8150e42188c0f95cb8c401a
mukhtar-syarief <mukhtar.syariefudin@gmail.com>
2022-05-19 07:45:41 +0700

    Coba DB dari data ujian Python

--- cobadb.py
+++ cobadb.py
@@ -1,17 +1,64 @@
+# IMPORT LIBBRARY SQLITE3
 import sqlite3
 
+# CONNECT KE DATABASE
 dbmahasiswa = sqlite3.connect("./db/mahasiswa.db")
 mahasiswa = dbmahasiswa.cursor()
 
+# MEMBUAT TABEL BARU DENGAN CREATE
 # mahasiswa.execute('''CREATE TABLE mahasiswa (nim integer, nama_mahasiswa text, jurusan text, tahun_masuk integer)''')
+
+# MENGECEK TABEL JIKA SUDAH ADA
+mahasiswa.execute("SHOW TABLES")
+
+# # 
 # mahasiswa.execute("INSERT INTO mahasiswa VALUES (1344201001, 'Babe Cabita', 'Akuntansi', 2013)")
 # mahasiswa.execute("INSERT INTO mahasiswa VALUES (1244201001, 'Budi Gunawan', 'Ilmu Komunikasi', 2012)")
 # mahasiswa.execute("INSERT INTO mahasiswa VALUES (1444201001, 'Dahlan Iskan', 'Teknik Informatika', 2014)")
 # mahasiswa.execute("INSERT INTO mahasiswa VALUES (1544201001, 'Budi Sadikin', 'Matematika', 2015)")
 
 
-for row in mahasiswa.execute("SELECT * FROM mahasiswa"):
-    print(row)
+# for row in mahasiswa.execute("SELECT * FROM mahasiswa"):
+#     print(row)
+
+# print(type(row))
+
+# data_mahasiswa = [{
+#     "nim" : 1444201005,
+#     "nama" : "Satya Dharma",
+#     "jurusan" : "Aktuaria",
+#     "tahun_masuk" : 2014
+#     },{
+#     "nim" : 1444201004,
+#     "nama" : "Tri Sturina",
+#     "jurusan" : "Aktuaria",
+#     "tahun_masuk" : 2014
+#     },{
+#     "nim" : 1444201003,
+#     "nama" : "Desi Ratnasari",
+#     "jurusan" : "Aktuaria",
+#     "tahun_masuk" : 2014
+#     },{
+#     "nim" : 1444201002,
+#     "nama" : "Ratna Wulandari",
+#     "jurusan" : "Aktuaria",
+#     "tahun_masuk" : 2014
+#     },{
+#     "nim" : 1444201001,
+#     "nama" : "Zoya Fitria",
+#     "jurusan" : "Aktuaria",
+#     "tahun_masuk" : 2014
+#     }]
+
+
+# for data in data_mahasiswa:
+#     print(data["nama"])
+#     mahasiswa.execute("INSERT INTO mahasiswa VALUES (?,?,?,?)", (data["nim"], data["nama"], data["jurusan"], data["tahun_masuk"]))
+# #     # mahasiswa.execute("INSERT INTO mahasiswa VALUES (data["nim"], data["nama"], data["jurusan"], data["tahun_masuk"])")
+
+
+# for row in mahasiswa.execute("SELECT * FROM mahasiswa"):
+#     print(row)
 
 
 dbmahasiswa.commit()
---
+++ cobadb2.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+dbkaryawan = sqlite3.connect('db/data_karyawan.db')
+data_karyawan = dbkaryawan.cursor()
+
+absen_karyawan = []
+with open("data/absen_karyawan.txt") as absensi:
+    absen = absensi.read().split('\n')
+    for absen in absen:
+        absen = absen.split(',')
+        absen = {
+            'nama': absen[0],
+            'waktu': absen[1],
+            'kehadiran':absen[2]
+        }
+        absen_karyawan.append(absen)
+
+# print(absen_karyawan)
+
+
+
+# data_karyawan.execute("CREATE TABLE absensi_karyawan (nama text, waktu text, kehadiran text)")
+
+# for absen in absen_karyawan:
+#     data_karyawan.execute("INSERT INTO absensi_karyawan VALUES(?, ?, ?)", (absen['nama'], absen['waktu'], absen['kehadiran']))
+
+# for x in data_karyawan.execute("SELECT * FROM absensi_karyawan"):
+#     print(x)
+
+data_order = []
+with open("data/data_order.txt") as order:
+    order = order.read().split(",")
+    for order in order:
+        order = order.split(",")
+        order = {
+            'nama': order[0],
+            'waktu': order[1],
+            'nama_produk': order[2],
+            'quantity': order[3]
+        }
+        data_order.append(order)
+
+data_karyawan.execute("CREATE TABLE data_order")
+
+dbkaryawan.commit()
+dbkaryawan.close()
